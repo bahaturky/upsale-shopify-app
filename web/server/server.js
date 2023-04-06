096925f610e692a06d24083c7148eb1ca1802150
bahaturky <bahaturkyilmaz@gmail.com>
2023-04-06 17:45:24 +0000

    add cors handler

--- web/server/server.js
+++ web/server/server.js
@@ -385,6 +385,25 @@ app.use("/widget/build/static/js/:filename", async (req, res) => {
     res.sendFile(req.params.filename, options);
 });
 
+app.use('/api/food', (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Website you wish to allow to connect
+    //res.setHeader("Access-Control-Allow-Origin", "http://localhost:3030");
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', '*');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+})
+
 app.post("/api/food", AnalyticsHandler.track);
 
 // All endpoints after this point will require an active session
